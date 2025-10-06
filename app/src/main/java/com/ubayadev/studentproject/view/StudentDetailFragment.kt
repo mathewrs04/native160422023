d82dece835a1c1f03115950e2949f0acdb3c5255
Mathew Reynald <mathew.reynald04@gmail.com>
2025-10-06 10:49:33 +0700

    tambah toast

--- app/src/main/java/com/ubayadev/studentproject/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubayadev/studentproject/view/StudentDetailFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ubayadev.studentproject.R
@@ -29,6 +30,8 @@ class StudentDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // baca id student lalu panggil fetch viewmodel
+        // untuk load data student tersebut
         val id = StudentDetailFragmentArgs.fromBundle(requireArguments()).id
         viewmodel = ViewModelProvider(this).get(DetailViewModel::class.java)
         viewmodel.fetch(id)
@@ -39,6 +42,7 @@ class StudentDetailFragment : Fragment() {
     fun observeViewModel() {
         viewmodel.studentLD.observe(viewLifecycleOwner, Observer {
             student = it
+            Toast.makeText(context, "Data Loaded", Toast.LENGTH_SHORT).show()
 
             // update UI
             binding.txtId.setText(student.id)

e657fb46ee3a7fcafe09af5f33d12c262c4e48ac
Mathew Reynald <mathew.reynald04@gmail.com>
2025-10-06 11:22:43 +0700

    add update function in DetailViewModel

--- app/src/main/java/com/ubayadev/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/ubayadev/studentproject/viewmodel/DetailViewModel.kt
@@ -17,6 +17,11 @@ class DetailViewModel(app:Application): AndroidViewModel(app) {
     val TAG = "vollyTag"
     private var queue: RequestQueue? = null
 
+    fun update(id: String) {
+        // diisi  coding untuk simpan studentLD object ke server
+        // pake volley
+    }
+
     fun fetch(id:String) {
         // volley ke API
         queue = Volley.newRequestQueue(getApplication())
